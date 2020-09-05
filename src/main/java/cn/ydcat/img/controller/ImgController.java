8f269c8e96b0103d7b3474946ee7927238def991
YD-Cat <704200551@qq.com>
2020-09-05 14:59:51 +0800

    Add

---
+++ src/main/java/cn/ydcat/img/controller/ImgController.java
@@ -0,0 +1,42 @@
+package cn.ydcat.img.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Random;
+
+@Controller
+public class ImgController {
+    // 本地图片路径
+    @Value("${img.path}")
+    private String path;
+    // 第三方随机图片接口
+    private String url = "https://api.ixiaowai.cn/api/api.php";
+
+    @GetMapping("/")
+    public String img(HttpServletResponse response){
+        // 设置页面不缓存
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        // 从图片库中随机获取一张图片
+        File file = new File(path);
+        File[] files = file.listFiles();
+        // 通过转发访问本地图片
+        if (files.length > 0) {
+            Random r = new Random();
+            int i = r.nextInt(files.length);
+            String name = files[i].getName();
+            url = "img/" + name;
+            return "forward:" + url;
+        } else {
+            // 没有配置本地图片，重定向到第三方
+            return "redirect:" + url;
+        }
+    }
+}
